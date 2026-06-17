be38ab2d8d12f03596939bf85e78795cba43b873
Gusales <dev.gussales@gmail.com>
2026-06-17 00:41:59 -0300

    feat(frontend): add information section for vessel registration with emission calculation details

--- apps/frontend/src/app/pages/cadastrar-embarcacao/cadastrar-embarcacao.ts
+++ apps/frontend/src/app/pages/cadastrar-embarcacao/cadastrar-embarcacao.ts
@@ -1,9 +1,12 @@
-import { Component } from "@angular/core";
-import { FormCriarEmbarcacao } from "./components/form-criar-embarcacao";
+import { Component } from '@angular/core';
+import { LucideAngularModule, Ship } from 'lucide-angular';
+import { FormCriarEmbarcacao } from './components/form-criar-embarcacao';
 
 @Component({
-    selector: 'app-cadastrar-embarcacao',
-    imports: [FormCriarEmbarcacao],
-    templateUrl: './cadastrar-embarcacao.html',
+  selector: 'app-cadastrar-embarcacao',
+  imports: [FormCriarEmbarcacao, LucideAngularModule],
+  templateUrl: './cadastrar-embarcacao.html',
 })
-export class CadastrarEmbarcacao { }
+export class CadastrarEmbarcacao {
+  public readonly ShipIcon = Ship;
+}
